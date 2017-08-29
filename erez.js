fde61a021bf9fc594485503065bf78d0c3436fa6
erezberezbot <31415468+erezberezbot@users.noreply.github.com>
2017-08-29 20:07:59 +0300

    Update erez.js

--- erez.js
+++ erez.js
@@ -7,107 +7,113 @@ client.on('ready', () => {
 
 let msgCount = 0;
 let test = false;
+let on = true;
 
 
 client.on('message', msg => {
+  if(on) {
+    let user = msg.author.username;
+    if(user == 'Erez Berez')
+      return false;
+    /*if(msg.author.username == 'Pecan')
+      msg.reply('תחזרי למטבח');*/
 
-  let user = msg.author.username;
+    let testChannel = msg.channel;
 
-  if(user == 'Erez Berez')
-    return false;
-  /*if(msg.author.username == 'Pecan')
-    msg.reply('תחזרי למטבח');*/
+      /*if(user == 'Electro Power') {
+        msg.reply('צא מהשרת ילד זין');
+        return false;
+      }*/
 
-  let testChannel = msg.channel;
-
-  	/*if(user == 'Electro Power') {
-  		msg.reply('צא מהשרת ילד זין');
-  		return false;
-  	}*/
-   
-  if(!test) {
-    setInterval(function() {
-      try {
-        let gchannel = msg.channel;
-        if(gchannel.name == 'general')
-          gchannel.send('אני הוא ארז, נוצרתי ע"י <@329255866882850816>');
-      } catch(e) {
-        console.log('got pm');
-      }
-    }, 3600000);
-    console.log('TEST');
-    test = true;
-  }
-  
-  
-  
-  if(msg.toString().includes('תודה מוריד'))
-    msg.reply('יש קראק?');
-
-  msgCount++;
-  if(msgCount % 30 == 0)
-    msg.reply(user == 'Pecan' ? ' תחזרי למטבח' :
-    'עכשיו מור תתן ירידה מעפנה ותנעל את האשכול');
-
-  if(msg.toString().includes('חמור'))
-    testChannel.send(user == 'Pecan' ? `מור יזונה
-https://www.youtube.com/watch?v=JSgeAFTwg0U`:
-    `מור, קו-קו-קו-קו-קוראים לי מור
-https://www.youtube.com/watch?v=JSgeAFTwg0U`);
-
-  if(msg.toString().includes('חרמן')) {
-    msg.reply('', {
-      file: "https://cdn.discordapp.com/attachments/347742914127986699/349680339817463808/dCrb1JO.png"
-    });
-  }
-
-  let random = Math.floor(Math.random() * 40);
-  console.log('random:', random);
-  if(random == 3) {
-    msg.reply(user == 'Pecan' ? 'קחי את הקרשים שלך את לא עושה איתנו לג בעומר':
-    'קח את הקרשים שלך אתה לא עושה איתנו לג בעומר');
-  }
-
-  if(msg.toString().includes('פמיניסט'))
-    msg.reply('תחזרי למטבח');
-
-  if(msg.toString().includes('ניקו ניקו ני'))
-    msg.reply('https://www.youtube.com/watch?v=latV0YAR-0s');
-
-  if(msg.toString().substr(0, 5) == 'ארז י')
-    msg.reply(`אמאשך ${msg.toString().substr(5)}`);
-
-
-  let msgs = msg.toString().split(' ');
-  let curses =
-  'זין&הומו&קוקסינל&בתול&ילד כספ&מוצץ זין&עומו&חמאר&ילד אנימה גיי&תתאבד'
-  .split('&');
-
-  function curse() {
-    let curse = '';
-    for(i = 0; i < 3; i++) {
-      let rand = curses[Math.floor(Math.random() * curses.length)];
-      if(curse.includes(rand)) {
-        i--;
-        continue;
-      }
-      curse += rand + ' ';
+    if(msg.server && msg.toString() == 'ארז סתום תפה' && msg.author.hasRole(msg.server.roles.get("name", "erezmaster"))) {
+      on = false;
+      testChannel.send('טוב נו, בי לבנתיים');
     }
-    return curse;
-  }
 
-  if(msg.toString().startsWith('ארז תקלל את ')) {
-    testChannel.send(`${msgs[3]} יא ${curse()}`);
-  }
-  
-  if(msg.toString().startsWith('ארז תגיד ')) {
-    testChannel.send(msg.toString().substr(9));
-  }
+    if(!test) {
+      setInterval(function() {
+        try {
+          let gchannel = msg.channel;
+          if(gchannel.name == 'general')
+            gchannel.send('אני הוא ארז, נוצרתי ע"י <@329255866882850816>');
+        } catch(e) {
+          console.log('got pm');
+        }
+      }, 3600000);
+      console.log('TEST');
+      test = true;
+    }
 
-  if(msg.toString() == 'ארז אתה אוהב אותי?')
-    msg.reply(`לא כי אתה ${curse()}`);
 
-  console.log('msgs:', msgCount);
+
+    if(msg.toString().includes('תודה מוריד'))
+      msg.reply('יש קראק?');
+
+    msgCount++;
+    if(msgCount % 30 == 0)
+      msg.reply(user == 'Pecan' ? ' תחזרי למטבח' :
+      'עכשיו מור תתן ירידה מעפנה ותנעל את האשכול');
+
+    if(msg.toString().includes('חמור'))
+      testChannel.send(user == 'Pecan' ? `מור יזונה
+  https://www.youtube.com/watch?v=JSgeAFTwg0U`:
+      `מור, קו-קו-קו-קו-קוראים לי מור
+  https://www.youtube.com/watch?v=JSgeAFTwg0U`);
+
+    if(msg.toString().includes('חרמן')) {
+      msg.reply('', {
+        file: "https://cdn.discordapp.com/attachments/347742914127986699/349680339817463808/dCrb1JO.png"
+      });
+    }
+
+    let random = Math.floor(Math.random() * 40);
+    console.log('random:', random);
+    if(random == 3) {
+      msg.reply(user == 'Pecan' ? 'קחי את הקרשים שלך את לא עושה איתנו לג בעומר':
+      'קח את הקרשים שלך אתה לא עושה איתנו לג בעומר');
+    }
+
+    if(msg.toString().includes('פמיניסט'))
+      msg.reply('תחזרי למטבח');
+
+    if(msg.toString().includes('ניקו ניקו ני'))
+      msg.reply('https://www.youtube.com/watch?v=latV0YAR-0s');
+
+    if(msg.toString().substr(0, 5) == 'ארז י')
+      msg.reply(`אמאשך ${msg.toString().substr(5)}`);
+
+
+    let msgs = msg.toString().split(' ');
+    let curses =
+    'זין&הומו&קוקסינל&בתול&ילד כספ&מוצץ זין&עומו&חמאר&ילד אנימה גיי&תתאבד'
+    .split('&');
+
+    function curse() {
+      let curse = '';
+      for(i = 0; i < 3; i++) {
+        let rand = curses[Math.floor(Math.random() * curses.length)];
+        if(curse.includes(rand)) {
+          i--;
+          continue;
+        }
+        curse += rand + ' ';
+      }
+      return curse;
+    }
+
+    if(msg.toString().startsWith('ארז תקלל את ')) {
+      testChannel.send(`${msgs[3]} יא ${curse()}`);
+    }
+
+    if(msg.toString().startsWith('ארז תגיד ')) {
+      testChannel.send(msg.toString().substr(9));
+    }
+
+    if(msg.toString() == 'ארז אתה אוהב אותי?')
+      msg.reply(`לא כי אתה ${curse()}`);
+
+    console.log('msgs:', msgCount);
+  }
 });
 
 client.login('MzQ5Njc0MzY1NzYxMDkzNjM0.DH47MA.MuODI2P8lJ2-JdXsbn3eJ0yk7w0');
